f85a31a4ee446de503fd27f5590eae35f3071bce
Alan Race <alan.m.race@gmail.com>
2015-11-02 19:26:23 +0000

    Tidied up code and added a couple of bits of Javadoc

--- src/main/java/com/alanmrace/jimzmlparser/mzML/Activation.java
+++ src/main/java/com/alanmrace/jimzmlparser/mzML/Activation.java
@@ -2,63 +2,96 @@ package com.alanmrace.jimzmlparser.mzML;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- * Activation tag. 
+ * Activation tag.
  *
  * @author Alan Race
  */
-public class Activation extends MzMLContent implements Serializable {
+public class Activation extends MzMLContent {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	/** Accession: Precursor activation attribute (MS:1000510). MAY be supplied (or any children) one or more times  */
-	public static String precursorActivationAttributeID	= "MS:1000510"; // Optional child (1+)
-	
-	/** Accession: Dissociation method (MS:1000044). MUST be supplied (or any children) one or more times */
-	public static String dissociationMethodID 			= "MS:1000044";	// Required child (1+)
-	
-	public Activation() {
-		super();
-	}
-	
-	public Activation(Activation activation, ReferenceableParamGroupList rpgList) {
-		super(activation, rpgList);
-	}
-	
-	public ArrayList<OBOTermInclusion> getListOfRequiredCVParams() {
-		ArrayList<OBOTermInclusion> required = new ArrayList<OBOTermInclusion>();
-		required.add(new OBOTermInclusion(dissociationMethodID, false, true, true));
-				
-		return required; 
-	}
-	
-	public ArrayList<OBOTermInclusion> getListOfOptionalCVParams() {
-		ArrayList<OBOTermInclusion> optional = new ArrayList<OBOTermInclusion>();
-		optional.add(new OBOTermInclusion(precursorActivationAttributeID, false, true, false));
-		
-		return optional;
-	}
-	
-	/* (non-Javadoc)
-	 * @see mzML.MzMLContent#outputXML(java.io.BufferedWriter, int)
-	 */
-	public void outputXML(BufferedWriter output, int indent) throws IOException {
-		MzMLContent.indent(output, indent);
-		output.write("<activation>\n");
-		
-		super.outputXML(output, indent+1);
-	
-		MzMLContent.indent(output, indent);
-		output.write("</activation>\n");
-	}
-	
-	public String toString() {
-		return "activation";
-	}
+    /**
+     * Accession: Precursor activation attribute (MS:1000510). MAY be supplied
+     * (or any children) one or more times
+     */
+    public static final String precursorActivationAttributeID = "MS:1000510"; // Optional child (1+)
+
+    /**
+     * Accession: Dissociation method (MS:1000044). MUST be supplied (or any
+     * children) one or more times
+     */
+    public static final String dissociationMethodID = "MS:1000044";	// Required child (1+)
+
+    /**
+     * Create an Activation tag
+     */
+    public Activation() {
+        super();
+    }
+
+    /**
+     *
+     * @param activation
+     * @param rpgList
+     */
+    public Activation(Activation activation, ReferenceableParamGroupList rpgList) {
+        super(activation, rpgList);
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public ArrayList<OBOTermInclusion> getListOfRequiredCVParams() {
+        ArrayList<OBOTermInclusion> required = new ArrayList<>();
+        required.add(new OBOTermInclusion(dissociationMethodID, false, true, true));
+
+        return required;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public ArrayList<OBOTermInclusion> getListOfOptionalCVParams() {
+        ArrayList<OBOTermInclusion> optional = new ArrayList<>();
+        optional.add(new OBOTermInclusion(precursorActivationAttributeID, false, true, false));
+
+        return optional;
+    }
+
+    /* (non-Javadoc)
+     * @see mzML.MzMLContent#outputXML(java.io.BufferedWriter, int)
+     */
+    /**
+     * Format the contents of the class into XML at a specified number of tab
+     * indents and output to a BufferedReader
+     *
+     * @param output BufferedReader to output the XML to
+     * @param indent Number of tabs to indent the XML
+     * @throws IOException Exception occurred during writing data
+     */
+    @Override
+    public void outputXML(BufferedWriter output, int indent) throws IOException {
+        MzMLContent.indent(output, indent);
+        output.write("<activation>\n");
+
+        super.outputXML(output, indent + 1);
+
+        MzMLContent.indent(output, indent);
+        output.write("</activation>\n");
+    }
+
+    @Override
+    public String toString() {
+        return "activation";
+    }
 }
